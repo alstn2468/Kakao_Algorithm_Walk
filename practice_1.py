bdb7a25fc86f81e3003afdd4c97755dc6cd2bfdd
alstn2468 <alstn2468_@naver.com>
2018-07-31 12:36:41 +0900

    2018-07-31

--- practice_1.py
+++ practice_1.py
@@ -11,24 +11,29 @@ b = 8888
 
 def sol(input) :
     input = str(input)
-    count = 0
 
-    for idx in input :
-        idx = int(idx)
-
-        if idx == 8 :
-            count += 1
-
-        else :
-            pass
-
-    return count
+    return input.count('8')
 
 
 if __name__ == '__main__' :
     total_sum = 0
 
+    print('- Solution Method Test -')
+    print(sol(8), sol(88), sol(888), sol(8888))
+    print(sol(80), sol(880), sol(8880))
+    '''
+    - Solution Method Test -
+    1 2 3 4
+    1 2 3
+    '''
+
+
     for i in range(10001) :
         total_sum += sol(i)
 
+    print('- From 1 to 10,000 each 8 count solution -')
     print(total_sum)
+    '''
+    - From 1 to 10,000 each 8 count solution -
+    4000
+    '''
