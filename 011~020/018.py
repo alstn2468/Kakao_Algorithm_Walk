fcab473be0aca2dfbaee13e79b7d6ee45db0b3b9
alstn2468 <alstn2468_@naver.com>
2018-08-02 15:15:08 +0900

    2018-08-02

--- 011~020/018.py
+++ 011~020/018.py
@@ -1,1 +1,29 @@
 # 018.py
+def get_min_idx(list) :
+    idx = 0
+
+    for i in range(1, len(list)) :
+        if list[i] < list[idx] :
+            idx = i
+
+    return idx
+
+def InsertionSort(list) :
+    result = []
+
+    while list :
+        temp = get_min_idx(list)
+        min = list.pop(temp)
+        result.append(min)
+
+    return result
+
+list = [1, 3, 2, 5, 4, 7, 6, 9, 8]
+
+print('- Before Sorting -')
+print(list)
+
+list = InsertionSort(list)
+
+print('- After Sorting -')
+print(list)
