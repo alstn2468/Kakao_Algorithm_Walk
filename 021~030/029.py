36df8b554e0e9dffee91ff7c6c01d4586394f920
alstn2468 <alstn2468_@naver.com>
2018-08-07 15:05:10 +0900

    2018-08-07

--- 021~030/029.py
+++ 021~030/029.py
@@ -3,7 +3,8 @@
 지도개발팀에서 근무하는 제이지는 지도에서 도시 이름을 검색하면
 해당 도시와 관련된 맛집 게시물들을 데이터베이스에서 읽어 보여주는 서비스를 개발하고 있다.
 이 프로그램의 테스팅 업무를 담당하고 있는 어피치는 서비스를 오픈하기 전
-각 로직에 대한 성능 측정을 수행하였는데, 제이지가 작성한 부분 중 데이터베이스에서 게시물을 가져오는 부분의 실행시간이 너무 오래 걸린다는 것을 알게 되었다.
+각 로직에 대한 성능 측정을 수행하였는데, 제이지가 작성한 부분 중
+데이터베이스에서 게시물을 가져오는 부분의 실행시간이 너무 오래 걸린다는 것을 알게 되었다.
 어피치는 제이지에게 해당 로직을 개선하라고 닦달하기 시작하였고,
 제이지는 DB 캐시를 적용하여 성능 개선을 시도하고 있지만 캐시 크기를 얼마로 해야
 효율적인지 몰라 난감한 상황이다.
@@ -19,10 +20,70 @@ cities는 도시 이름으로 이뤄진 문자열 배열로,
 대소문자 구분을 하지 않는다. 도시 이름은 최대 20자로 이루어져 있다.
 
 출력 형식
-입력된 도시이름 배열을 순서대로 처리할 때, “총 실행시간”을 출력한다.
+입력된 도시이름 배열을 순서대로 처리할 때, "총 실행시간"을 출력한다.
 
 조건
 캐시 교체 알고리즘은 LRU(Least Recently Used)를 사용한다.
 cache hit일 경우 실행시간은 1이다.
 cache miss일 경우 실행시간은 5이다.
 '''
+
+cacheSize = [3, 3, 2, 5, 2, 0]
+cities = [["Jeju", "Pangyo", "Seoul",
+          "NewYork", "LA", "Jeju",
+           "Pangyo", "Seoul", "NewYork", "LA"],
+
+          ["Jeju", "Pangyo", "Seoul",
+           "Jeju", "Pangyo", "Seoul",
+           "Jeju", "Pangyo", "Seoul"],
+
+          ["Jeju", "Pangyo", "Seoul",
+           "NewYork", "LA", "SanFrancisco",
+           "Seoul", "Rome", "Paris",
+           "Jeju", "NewYork", "Rome"],
+
+          ["Jeju", "Pangyo", "Seoul",
+           "NewYork", "LA", "SanFrancisco",
+           "Seoul", "Rome", "Paris",
+           "Jeju", "NewYork", "Rome"],
+
+          ["Jeju", "Pangyo",
+           "NewYork", "newyork"],
+
+          ["Jeju", "Pangyo", "Seoul",
+           "NewYork", "LA"]]
+
+def LRU(cacheSize, cache, newinput, idx) :
+    if len(cache) == cacheSize :
+        cache[idx] = newinput
+
+    else :
+        cache.append(newinput)
+
+    return cache
+
+def sol(cacheSize, cities) :
+    if cacheSize == 0 :
+        return len(cities) * 5
+
+    else :
+        result_time = 0
+        cache = []
+
+        for i in range(len(cities)) :
+            cities[i] = cities[i].lower()
+
+        for i in range(len(cities)) :
+            idx = i % cacheSize
+
+            if cities[i] in cache :
+                result_time += 1
+
+            else :
+                cache = LRU(cacheSize, cache, cities[i], idx)
+                result_time += 5
+
+        return result_time
+
+for i in range(len(cacheSize)) :
+    print(sol(cacheSize[i], cities[i]))
